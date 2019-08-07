86c448c57e26cdbcf2792fafa753eb9bc15c0a52
Jagdeep-dev <53865859+Jagdeep-dev@users.noreply.github.com>
2019-08-07 19:05:19 +0530

    String In Reverse Order
    Reverse order of string in python program

---
+++ String_In_ReverseOrder.py
@@ -0,0 +1,11 @@
+# Please write a program which accepts a string from console and print it in reverse order.
+
+#---Solution:
+
+s=input()
+
+s = s[::-1]
+
+print(s)
+
+#--Use list[::-1] to iterate a list in a reverse order
